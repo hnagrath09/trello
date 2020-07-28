dce1f764cd8fd379e7072fd7cdd1b890a6e33a01
hnagrath09 <him.nagrath@gmail.com>
2020-07-28 21:49:37 +0530

    Functionality to drag lists and drop cards

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
 import Card from "./Card";
 import CreateCard from "./CreateCard";
@@ -35,7 +36,7 @@ const List: React.FC<Props> = ({ list, updateTitle }) => {
       const newList = {
         id: Date.now(),
         parentId: list.id,
-        order: card.length + 1,
+        order: card.length,
         title: title,
         description: "",
       };
@@ -58,54 +59,72 @@ const List: React.FC<Props> = ({ list, updateTitle }) => {
   };
 
   return (
-    <div className="w-64 pt-2 mx-2 bg-gray-300 rounded-sm ">
-      <div className="flex items-center justify-between">
-        {editListTitle ? (
-          <form
-            onSubmit={(event) => {
-              event.preventDefault();
-              updateTitle(listTitle, list.id);
-              setEditListTitle(false);
-            }}
-          >
-            <input
-              className="pl-1 ml-3 text-sm font-semibold text-gray-700 border-2 border-blue-600 rounded-sm focus:outline-none"
-              autoFocus
-              onFocus={(event) => event.target.select()}
-              onChange={(event: {
-                target: { value: React.SetStateAction<string> };
-              }) => setListTitle(event.target.value)}
-              defaultValue={listTitle}
-            ></input>
-          </form>
-        ) : (
+    <Draggable draggableId={list.id.toString()} index={list.order}>
+      {(provided) => (
+        <div
+          className="w-64 pt-2 mx-2 bg-gray-300 rounded-sm "
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+        >
           <div
-            className="w-full ml-3 text-sm font-semibold text-gray-800 cursor-pointer"
-            onClick={() => {
-              setEditListTitle(true);
-            }}
+            className="flex items-center justify-between"
+            {...provided.dragHandleProps}
           >
-            {list.title}
-          </div>
-        )}
+            {editListTitle ? (
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  updateTitle(listTitle, list.id);
+                  setEditListTitle(false);
+                }}
+              >
+                <input
+                  className="pl-1 ml-3 text-sm font-semibold text-gray-700 border-2 border-blue-600 rounded-sm focus:outline-none"
+                  autoFocus
+                  onFocus={(event) => event.target.select()}
+                  onChange={(event: {
+                    target: { value: React.SetStateAction<string> };
+                  }) => setListTitle(event.target.value)}
+                  defaultValue={listTitle}
+                ></input>
+              </form>
+            ) : (
+              <div
+                className="w-full ml-3 text-sm font-semibold text-gray-800 cursor-pointer"
+                onClick={() => {
+                  setEditListTitle(true);
+                }}
+              >
+                {list.title}
+              </div>
+            )}
 
-        <div className="p-1 mr-1 rounded-sm cursor-pointer hover:bg-gray-500">
-          <HorizontalDotsIcon className="w-4 h-4" />
+            <div className="p-1 mr-1 rounded-sm cursor-pointer hover:bg-gray-500">
+              <HorizontalDotsIcon className="w-4 h-4" />
+            </div>
+          </div>
+          <Droppable droppableId={list.id.toString()}>
+            {(provided) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {card.map((task: Card) =>
+                  task.parentId === list.id ? (
+                    <Card
+                      key={task.id}
+                      card={task}
+                      column={list}
+                      updateCardDes={handleCardDescription}
+                      updateCardTit={handleCardTitle}
+                    />
+                  ) : undefined
+                )}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          <CreateCard getTitle={handleCreateCard} />
         </div>
-      </div>
-      {card.map((task: Card) =>
-        task.parentId === list.id ? (
-          <Card
-            key={task.id}
-            card={task}
-            column={list}
-            updateCardDes={handleCardDescription}
-            updateCardTit={handleCardTitle}
-          />
-        ) : undefined
       )}
-      <CreateCard getTitle={handleCreateCard} />
-    </div>
+    </Draggable>
   );
 };
 
