34d7299542d36e1995d2664cacb8b89194282043
hnagrath09 <him.nagrath@gmail.com>
2020-07-28 21:50:09 +0530

    Functionality to drag cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
 
 import Modal from "./Modal";
 import PencilIcon from "./icons/PencilIcon";
@@ -53,18 +54,26 @@ const Card: React.FC<Props> = ({
 
   return (
     <>
-      <div
-        className="px-2 py-1 mx-2 mt-2 text-sm bg-gray-100 shadow cursor-pointer group"
-        onClick={showModal}
-      >
-        <div className="flex items-center justify-between">
-          {card.title}
-          <PencilIcon className="hidden w-3 h-3 group-hover:block" />
-        </div>
-        {card.description ? (
-          <MenuAlt2Icon className="w-4 h-4 my-1" />
-        ) : undefined}
-      </div>
+      <Draggable draggableId={card.id.toString()} index={card.order}>
+        {(provided) => (
+          <div
+            className="px-2 py-1 mx-2 mt-2 text-sm bg-gray-100 shadow cursor-pointer group"
+            onClick={showModal}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <div className="flex items-center justify-between">
+              {card.title}
+              <PencilIcon className="hidden w-3 h-3 group-hover:block" />
+            </div>
+            {card.description ? (
+              <MenuAlt2Icon className="w-4 h-4 my-1" />
+            ) : undefined}
+          </div>
+        )}
+      </Draggable>
+
       <Modal
         show={open}
         handleCancel={hideModal}
