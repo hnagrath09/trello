73c2554d5627b8264b582ad1270da7daeee8ccf1
hnagrath09 <him.nagrath@gmail.com>
2020-07-31 13:18:24 +0530

    api connection for lists completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-
-import useLocalStorage from "./hooks/useLocalStorage";
+import { useQuery, useMutation, queryCache } from "react-query";
 
 import List from "./components/List";
 import CreateList from "./components/CreateList";
 
 import HomeIcon from "./components/icons/HomeIcon";
+import { fetchLists, createList, updateList } from "./queries/listQueries";
 
 interface List {
   id: number;
@@ -15,26 +15,43 @@ interface List {
 }
 
 const App = () => {
-  const [list, setList] = useLocalStorage("list", []);
+  const { isLoading, data: list, error } = useQuery("lists", fetchLists);
 
-  const handleCreateList = (title: string) => {
-    if (title) {
-      const newList = {
-        id: Date.now(),
-        order: list.length,
-        title: title,
-      };
-      setList((prevList: List[]) => [...prevList, newList]);
-    }
-  };
+  const [addList] = useMutation(createList, {
+    onSuccess: (createdList) => {
+      queryCache.setQueryData(
+        "lists",
+        list ? [...list, createdList] : [createdList]
+      );
+    },
+  });
 
-  const handleListTitle = (title: string, listId: number) => {
-    setList((prevList: List[]) =>
-      prevList.map((column) =>
-        column.id === listId ? { ...column, title } : column
-      )
-    );
-  };
+  const [editList] = useMutation(updateList, {
+    onSuccess: (updatedList) => {
+      queryCache.setQueryData(
+        "lists",
+        list?.map((column) =>
+          column.id === updatedList.id ? updatedList : column
+        )
+      );
+    },
+  });
+
+  const handleCreateList = useCallback(
+    (title: string) => {
+      if (title) {
+        addList({ title, order: list?.length ?? 0 });
+      }
+    },
+    [addList, list]
+  );
+
+  const handleListTitle = useCallback(
+    (title: string, listId: number) => {
+      editList({ title, id: listId });
+    },
+    [editList]
+  );
 
   const handleDrag = (result: any) => {
     const { destination, source, type } = result;
@@ -51,15 +68,48 @@ const App = () => {
     }
 
     if (type === "list") {
-      const newList = [...list];
-      const [item] = newList.slice(source.index, source.index + 1);
-      newList.splice(source.index, 1);
-      newList.splice(destination.index, 0, item);
-      newList.map((column, index) => (column.order = index));
-      setList(newList);
-      return;
+      console.log(type);
+      // const newList = [...list];
+      // const [item] = newList.slice(source.index, source.index + 1);
+      // newList.splice(source.index, 1);
+      // newList.splice(destination.index, 0, item);
+      // newList.map((column, index) => (column.order = index));
+      // setList(newList);
+      // return;
     }
   };
+
+  const Board = useMemo(() => {
+    return isLoading ? (
+      <div className="max-w-xs mx-auto my-64 text-lg font-semibold text-white">
+        Loading...
+      </div>
+    ) : error ? (
+      <div>{error.message}</div>
+    ) : (
+      <DragDropContext onDragEnd={handleDrag}>
+        <Droppable droppableId="all-columns" direction="horizontal" type="list">
+          {(provided) => (
+            <div
+              className="flex items-start "
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {list?.map((column: List) => (
+                <List
+                  key={column.id}
+                  list={column}
+                  updateTitle={handleListTitle}
+                />
+              ))}
+              {provided.placeholder}
+              <CreateList getTitle={handleCreateList} />
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }, [isLoading, error, handleCreateList, handleListTitle, list]);
 
   return (
     <div className="w-screen h-screen bg-blue-600 ">
@@ -76,27 +126,7 @@ const App = () => {
       {/* NavBar ending */}
 
       {/* Board starting */}
-      <DragDropContext onDragEnd={handleDrag}>
-        <Droppable droppableId="all-columns" direction="horizontal" type="list">
-          {(provided) => (
-            <div
-              className="flex items-start "
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              {list.map((column: List) => (
-                <List
-                  key={column.id}
-                  list={column}
-                  updateTitle={handleListTitle}
-                />
-              ))}
-              {provided.placeholder}
-              <CreateList getTitle={handleCreateList} />
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+      {Board}
       {/* Board ending */}
     </div>
   );
