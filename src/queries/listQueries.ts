764fd6bf1bba9a4c6da04c468bc242bb69c8ab13
hnagrath09 <him.nagrath@gmail.com>
2020-07-31 12:35:52 +0530

    CRUD operations for lists

---
+++ src/queries/listQueries.ts
@@ -0,0 +1,41 @@
+import Axios from "axios";
+
+interface List {
+  id: number;
+  order: number;
+  title: string;
+}
+
+const client = Axios.create({
+  baseURL: "http://localhost:1337",
+});
+
+export const fetchLists = async () => {
+  const { data } = await client.get<List[]>("/lists");
+  return data;
+};
+
+// Save new list using post request
+export const createList = async ({
+  title,
+  order,
+}: {
+  title: string;
+  order: number;
+}) => {
+  const { data } = await client.post("/lists", { title, order });
+  return data;
+};
+
+export const updateList = async ({
+  id,
+  title,
+  order,
+}: {
+  id: number;
+  title?: string;
+  order?: number;
+}) => {
+  const { data } = await client.put(`/lists/${id}`, { title, order });
+  return data;
+};
