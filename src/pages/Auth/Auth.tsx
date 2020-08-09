e7a759cec7847973452cd73adef0ff9f79b1278c
hnagrath09 <him.nagrath@gmail.com>
2020-08-09 19:55:47 +0530

    Added route for forgot password

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -3,6 +3,7 @@ import { Switch, Redirect, Route } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import ForgotPassword from "./components/ForgotPassword";
 
 const Auth = () => {
   const { user } = useContext(AuthContext);
@@ -16,6 +17,7 @@ const Auth = () => {
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route path="/reset-password" component={ForgotPassword} />
       </Switch>
     </div>
   );
