a37b41f8e6c6672d75306132e3c018e2b222bbf9
hnagrath09 <him.nagrath@gmail.com>
2020-08-09 19:56:23 +0530

    Completed login component

--- src/pages/Auth/components/Login.tsx
+++ src/pages/Auth/components/Login.tsx
@@ -1,51 +1,86 @@
-import React from "react";
+import React, { useContext, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
+import { Input, Checkbox, Button, Form } from "antd";
+import AuthContext from "../../../contexts/AuthContext";
 
 const Login = () => {
+  const { signInWithEmail } = useContext(AuthContext);
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = useCallback(
+    async ({ email, password, rememberMe }) => {
+      setLoading(true);
+      if (!(await signInWithEmail({ email, password, rememberMe }))) {
+        setLoading(false);
+      }
+    },
+    [signInWithEmail]
+  );
+
   return (
     <div className="max-w-md px-16 py-12 mx-auto bg-white rounded-sm shadow-lg ">
-      <div className="mb-8 font-bold text-center text-gray-700">
-        Log in to trello
+      <div className="space-y-4">
+        <div className="relative z-10 mb-6 text-xl font-bold text-center text-gray-800">
+          Log in to <span className="text-blue-500">trello</span>
+        </div>
       </div>
-      <form className="flex-col ">
-        <input
-          className="w-full px-2 py-2 mx-auto mb-6 text-sm bg-gray-100 border-2 border-gray-300 "
-          type="email"
-          placeholder="Enter email"
-        />
-        <input
-          className="w-full px-2 py-2 mx-auto mb-6 text-sm bg-gray-100 border-2 border-gray-300 "
-          type="password"
-          placeholder="Enter password"
-        />
-        <Link to="/">
-          <button
-            className="w-full py-2 mb-8 text-sm font-bold text-white bg-green-500 rounded-sm"
-            type="submit"
-          >
-            Log in
-          </button>
-        </Link>
-      </form>
-      <div className="mb-6 text-sm font-hairline text-center">OR</div>
-
-      <button
-        className="w-full py-2 mb-3 text-sm font-bold text-gray-600 bg-white border rounded-sm shadow"
-        type="submit"
+      <Form
+        layout="vertical"
+        colon={false}
+        onFinish={handleSubmit}
+        name="login"
+        initialValues={{ rememberMe: true }}
       >
-        Continue with Google
-      </button>
-
-      <div className="pt-4 mt-4 text-sm text-center text-blue-500 border-t-2">
-        <span className="mr-4 cursor-pointer hover:underline">
-          Can't log in?
-        </span>
-        <Link to="/signup">
-          <span className="cursor-pointer hover:underline">
-            Sign up for an account
-          </span>
-        </Link>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: "Email is required" },
+            {
+              type: "email",
+              message: "Email is invalid",
+              validateTrigger: "onblur",
+            },
+          ]}
+        >
+          <Input placeholder="Email" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Email is required" }]}
+        >
+          <Input.Password placeholder="Password" />
+        </Form.Item>
+        <div className="flex items-center justify-between mb-4">
+          <Form.Item name="rememberMe" valuePropName="checked" className="mb-0">
+            <Checkbox>Remember Me</Checkbox>
+          </Form.Item>
+          <Link
+            to="/reset-password"
+            className="text-sm font-medium text-center text-blue-500"
+          >
+            Forgot Password?
+          </Link>
+        </div>
+        <Button
+          className="w-full"
+          type="primary"
+          htmlType="submit"
+          loading={loading}
+        >
+          Login
+        </Button>
+      </Form>
+      <div className="relative mt-4 border-b border-gray-200">
+        <div className="mb-4 text-center">OR</div>
       </div>
+      <Link to="/signup">
+        <Button className="w-full" type="default">
+          Sign up for an account
+        </Button>
+      </Link>
     </div>
   );
 };
