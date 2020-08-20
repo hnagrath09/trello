adcf5be2020049e28eb6c73198955600d84c7507
hnagrath09 <him.nagrath@gmail.com>
2020-08-20 22:13:10 +0530

    Added new query createBoard

--- src/queries/boardQueries.ts
+++ src/queries/boardQueries.ts
@@ -8,3 +8,15 @@ export const fetchBoards = async () => {
   const { data } = await client.get("/boards");
   return data;
 };
+
+export const createBoard = async ({
+  userId,
+}: {
+  userId: string | undefined;
+}) => {
+  const { data } = await client.post("/boards", {
+    title: "Untitled board",
+    users: { _id: userId },
+  });
+  return data;
+};
