6f490d8578507b930fb2f587155a1b41152b0888
hnagrath09 <him.nagrath@gmail.com>
2020-08-21 21:36:49 +0530

    Now creating new board will automatically open the newly created board

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,21 +3,22 @@ import { Card, Button } from "antd";
 import "antd/dist/antd.css";
 import { useQuery, useMutation, queryCache } from "react-query";
 import { fetchBoards, createBoard } from "../../queries/boardQueries";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
 
 interface Board {
   _id: string;
   title: string;
 }
-const Home = () => {
-  const { isLoading, data: board } = useQuery("boards", fetchBoards);
-  const [addBoard] = useMutation(createBoard, {
+const Home = ({ history }: { history: any }) => {
+  const { data: board } = useQuery("boards", fetchBoards);
+  const [addBoard, { isLoading }] = useMutation(createBoard, {
     onSuccess: (createdBoard) => {
       queryCache.setQueryData(
         "boards",
         board ? [...board, createdBoard] : [createdBoard]
       );
+      history.push(`/boards/${createdBoard._id}`);
     },
   });
   const { Meta } = Card;
@@ -35,7 +36,12 @@ const Home = () => {
           <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
         </svg>
         <div>
-          <Button type="primary" className="mr-6" onClick={handleCreateBoard}>
+          <Button
+            type="primary"
+            className="mr-6"
+            loading={isLoading}
+            onClick={handleCreateBoard}
+          >
             New Board
           </Button>
           <span className="p-2 mr-2 text-sm font-semibold text-white bg-blue-400 rounded-full cursor-pointer">
@@ -50,7 +56,6 @@ const Home = () => {
               hoverable
               style={{ width: 240 }}
               className="mr-6"
-              loading={isLoading}
               cover={
                 <img
                   alt="example"
@@ -67,4 +72,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default withRouter(Home);
